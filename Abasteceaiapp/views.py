237cf4ea1ca487d998989ce825e4ac14e26131c6
Gabriel Petillo <gptm112@gmail.com>
2026-06-27 20:25:26 -0300

    contiuação de implementação dos updates + migration (possui erros)

--- Abasteceaiapp/views.py
+++ Abasteceaiapp/views.py
@@ -53,29 +53,23 @@ def adicionar_posto_view(request):
             endereco=endereco,
             latitude=latitude,
             longitude=longitude,
-
         )
         return render(request, 'detalhes.html', {'posto': posto})
     else:
-        return render(request, 'forms_posto.html')
+        return render(request, 'forms_posto.html',context={'action': 'Adicionar'})
     
-def atualizar_posto_view(request):
+def atualizar_posto_view(request,posto_id):
+    posto = get_object_or_404(Posto, id=posto_id)
     if request.method == 'POST':
-        nome = request.POST.get('nome')
-        endereco = request.POST.get('endereco')
-        latitude = request.POST.get('latitude')
-        longitude = request.POST.get('longitude')
-
-        posto = Posto.objects.create(
-            nome=nome,
-            endereco=endereco,
-            latitude=latitude,
-            longitude=longitude,
-
+        posto = Posto.objects.update(
+            nome=request.POST.get('nome'),
+            endereco=request.POST.get('endereco'),
+            latitude=request.POST.get('latitude'),
+            longitude=request.POST.get('longitude'),
         )
         return render(request, 'detalhes.html', {'posto': posto})
     else:
-        return render(request, 'forms_posto.html')
+        return render(request, 'forms_posto.html', context={'action': 'Atualizar'})
     
 def adicionar_preco_view(request):
     if request.method == 'POST':
@@ -89,21 +83,21 @@ def adicionar_preco_view(request):
         )
         return render(request, 'detalhes.html', {'preco': preco})
     else:
-        return render(request, 'forms_preco.html', context={'postos': Posto.objects.all()})
+        return render(request, 'forms_preco.html', context={'action': 'Adicionar', 'postos': Posto.objects.all()})
 
 def atualizar_preco_view(request):
     if request.method == 'POST':
         tipo_combustivel = request.POST.get('tipo_combustivel')
         preco = request.POST.get('valor')
 
-        preco = Preco.objects.create(
+        preco = Preco.objects.update(
             tipo_combustivel=tipo_combustivel,
             valor=preco,
             posto=get_object_or_404(Posto, id=request.POST.get('posto')),
         )
         return render(request, 'detalhes.html', {'preco': preco})
     else:
-        return render(request, 'forms_preco.html', context={'postos': Posto.objects.all()})   
+        return render(request, 'forms_preco.html', context={'action': 'Atualizar', 'postos': Posto.objects.all()})   
 
 def adicionar_comodidade_view(request):
     if request.method == 'POST':
@@ -118,14 +112,14 @@ def adicionar_comodidade_view(request):
         return render(request, 'detalhes.html', {'comodidade': comodidade})
     else:
         postos = Posto.objects.all()
-        return render(request, 'forms_comodidade.html', context={'postos': postos})
+        return render(request, 'forms_comodidade.html', context={'action': 'Adicionar','postos': postos})
     
 def atualizar_comodidade_view(request):
     if request.method == 'POST':
         tipo = request.POST.get('tipo')
         nome = request.POST.get('nome')
     
-        comodidade = Comodidade.objects.create(
+        comodidade = Comodidade.objects.update(
             posto=get_object_or_404(Posto, id=request.POST.get('posto')),
             tipo=tipo,
             nome=nome,
@@ -133,7 +127,7 @@ def atualizar_comodidade_view(request):
         return render(request, 'detalhes.html', {'comodidade': comodidade})
     else:
         postos = Posto.objects.all()
-        return render(request, 'forms_comodidade.html', context={'postos': postos})
+        return render(request, 'forms_comodidade.html', context={'action': 'Atualizar', 'postos': postos})
     
         
 
